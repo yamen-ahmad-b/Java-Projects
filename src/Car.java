7d850f078070866a623dfd7ffa0eb16dab41b037
yamen-ahmad-b <yamensy439@gmail.com>
2025-08-27 19:22:10 +0300

    Add MyFirstOOP project

---
+++ src/Car.java
@@ -0,0 +1,39 @@
+public class Car {
+
+String make;
+String model;
+String color;
+int year;
+double price;
+
+    void showInfo() {
+        System.out.println(year + " " + make + " " + model + " " + color + " $" + price);
+    }
+
+    void start() {
+        System.out.println(make + " " + model + " is moving");
+    }
+
+    void stop() {
+        System.out.println(make + " " + model + " has stopped");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+Car Toyota = new Car();
+Toyota.make="Toyota";
+Toyota.model ="Elantra";
+Toyota.color="black";
+Toyota.year= 2025;
+Toyota.price =23025;
+
+        Car nissan = new Car();
+        nissan.make="nissan";
+        nissan.model ="Pathfinder";
+        nissan.color="red";
+        nissan.year= 2025;
+        nissan.price =38000;
+
+nissan.start();
+nissan.stop();
+Toyota.start();
+Toyota.stop();
+nissan.showInfo();
+Toyota.showInfo();
+
+
+
+        }
+    }
