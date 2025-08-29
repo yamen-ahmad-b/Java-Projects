b5dbbeaf6ad32fe5bbb6fbad00e6e410b3c1c516
yamen ahmad <yamensy439@gmail.com>
2025-08-29 17:28:48 +0300

    Add files via upload

---
+++ University Manager/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+Student s1 = new Student("yamen",30,3.2);
+Student s2 = new Student("pstk",12,1.2);
+
+
+System.out.println(s1.name);
+System.out.println(s1.age);
+System.out.println(s1.gpa);
+System.out.println(s1.isEnrolled);
+System.out.println(s2.name);
+System.out.println(s2.age);
+System.out.println(s2.gpa);
+System.out.println(s2.isEnrolled);
+
+
+
+
+
+    }
+}
---
+++ University Manager/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+
+String name ;
+int age;
+double gpa;
+boolean isEnrolled;
+
+
+    Student(String name,int age,double gpa){
+this.name=name;
+this.age=age;
+this.gpa=gpa;
+this.isEnrolled=true;
+    }
+
+}
